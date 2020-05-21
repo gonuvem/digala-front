c98aa833b59a0551cc473f2cb46df2f1ed60b9fd
ppalbuquerque <albpedro897@hotmail.com>
2020-05-21 14:40:55 -0300

    Created folder structure for pages and components

---
+++ src/components/ShortTextField/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ShortTextField: React.FC = () => <div />;
+
+export default ShortTextField;
---
+++ src/pages/SurveyBuilder/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import ShortTextField from '../../components/ShortTextField';
+
+const SurveyBuilder: React.FC = () => (
+  <Container>
+    <ShortTextField />
+  </Container>
+);
+
+export default SurveyBuilder;
