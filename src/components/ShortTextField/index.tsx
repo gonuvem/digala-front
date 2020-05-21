e08d79e7cfd384349a3493fd23ab745e930e6ff1
ppalbuquerque <albpedro897@hotmail.com>
2020-05-21 14:52:06 -0300

    Added roboto font to project

--- src/components/ShortTextField/index.tsx
+++ src/components/ShortTextField/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const ShortTextField: React.FC = () => <div />;
+import { Container } from './styles';
+
+const ShortTextField: React.FC = () => (
+  <Container>
+    <label htmlFor="shortTextField">
+      Texto Curto
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+      <input type="text" id="shortTextField" />
+    </label>
+  </Container>
+);
 
 export default ShortTextField;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,4 +8,9 @@ export default createGlobalStyle`
     outline: 0;
   }
 
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+  }
 `;
