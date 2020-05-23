8cf4b628cc95fe981af9c3d52c024d5bd130c9fe
ppalbuquerque <albpedro897@hotmail.com>
2020-05-23 17:35:46 -0300

    Added superior tab navigation without animation

--- src/pages/SurveyBuilder/LeftPanel/index.tsx
+++ src/pages/SurveyBuilder/LeftPanel/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-import { Container, QuestionsContainer } from './styles';
+import { Container, QuestionsContainer, PanelTabLink } from './styles';
 
-const LeftPanel: React.FC = () => (
-  <Container>
-    <nav>
-      <span>Tipos</span>
-      <span>Estilos</span>
-      <span>Configurações</span>
-    </nav>
-    <div>
-      <h5>Perguntas Básicas</h5>
-      <QuestionsContainer />
-    </div>
-  </Container>
-);
+const LeftPanel: React.FC = () => {
+  const [activePanelNumber, setActivePanelNumber] = useState(0);
+
+  const handleTabChange = useCallback((tab) => {
+    setActivePanelNumber(tab);
+  }, []);
+
+  return (
+    <Container activePanelNumber={activePanelNumber}>
+      <nav>
+        <PanelTabLink
+          isActive={activePanelNumber === 0}
+          onClick={() => handleTabChange(0)}
+        >
+          Tipos
+        </PanelTabLink>
+        <PanelTabLink
+          isActive={activePanelNumber === 1}
+          onClick={() => handleTabChange(1)}
+        >
+          Estilos
+        </PanelTabLink>
+        <PanelTabLink
+          isActive={activePanelNumber === 2}
+          onClick={() => handleTabChange(2)}
+        >
+          Configurações
+        </PanelTabLink>
+      </nav>
+      <div>
+        <h5>Perguntas Básicas</h5>
+        <QuestionsContainer />
+      </div>
+    </Container>
+  );
+};
 
 export default LeftPanel;
--- src/pages/SurveyBuilder/LeftPanel/styles.ts
+++ src/pages/SurveyBuilder/LeftPanel/styles.ts
@@ -1,33 +1,66 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { transparentize, shade } from 'polished';
 
 import Colors from '../../../utils/colors';
 
-export const Container = styled.div`
+interface ContainerProps {
+  activePanelNumber: number;
+}
+
+interface PanelTabLinkProps {
+  isActive?: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background-color: ${Colors.white};
   border-radius: 4px;
 
   nav {
     display: flex;
+    position: relative;
     justify-content: space-between;
     padding: 2rem 2rem 1rem 2rem;
     margin-bottom: 1.5rem;
     border-bottom: solid 1px ${transparentize(0.88, Colors.black)};
 
-    span {
-      cursor: pointer;
-      font-weight: 500;
-      transition: color 0.3s;
+    &::after {
+      content: '';
+      width: 4rem;
+      position: absolute;
+      height: 2px;
+      background-color: ${Colors.primary};
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
 
-      &:hover {
-        color: ${shade(0.2, Colors.primary)};
-      }
+    div {
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      position: absolute;
     }
   }
 
   > div {
     padding: 0 2rem 2rem 2rem;
   }
+`;
+
+export const PanelTabLink = styled.span<PanelTabLinkProps>`
+  cursor: pointer;
+  position: relative;
+  font-weight: 500;
+  transition: color 0.3s;
+
+  &:hover {
+    color: ${shade(0.2, Colors.primary)};
+  }
+
+  ${(props) => props.isActive
+    && css`
+      color: ${shade(0.2, Colors.primary)};
+    `}
 `;
 
 export const QuestionsContainer = styled.div``;
