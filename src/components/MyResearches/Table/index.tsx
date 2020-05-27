2dec1217df80c009c6c7faef9317687870e9a6d1
Francisco Mendes <fmendes_cn@hotmail.com>
2020-05-27 14:52:26 -0300

    Get user forms from api

--- src/components/MyResearches/Table/index.tsx
+++ src/components/MyResearches/Table/index.tsx
@@ -17,53 +17,74 @@ import {
 import edit from '../../../assets/edit_icon.png';
 import trash from '../../../assets/trash_icon.png';
 
-const Table: React.FC = () => (
-  <>
-    <TableLabels>
-      <Name>
-        <p>NOME</p>
-      </Name>
-      <CreatedAt>
-        <p>CRIADO EM</p>
-      </CreatedAt>
-      <Answers>
-        <p>RESPOSTAS</p>
-      </Answers>
-      <Status>
-        <p>STATUS</p>
-      </Status>
-      <Actions>
-        <p>AÇÕES</p>
-      </Actions>
-    </TableLabels>
-    <TableRow>
-      <Name>
-        <p>Pesquisa eleitoral de Lagoa Alegre</p>
-      </Name>
-      <CreatedAt>
-        <p>22/09/2019</p>
-      </CreatedAt>
-      <Answers>
-        <p>12 envios</p>
-      </Answers>
-      <Status>
-        <ColorStatus isActive={true} />
-        <p>Ativa</p>
-      </Status>
-      <Actions>
-        <a href="/">
-          <img src={edit} alt="Editar" />
-          <EditLabel>Editar</EditLabel>
-        </a>
-        <div />
-        <a href="/">
-          <img src={trash} alt="Deletar" />
-          <DeleteLabel>Deletar</DeleteLabel>
-        </a>
-      </Actions>
-    </TableRow>
-    <Separator />
-  </>
-);
+interface TableProps {
+  forms: [
+    {
+      _id: string;
+      numResponses: number;
+      isActive: boolean;
+      createdAt: Date;
+      config: {
+        name: string;
+      };
+    },
+  ];
+}
+
+const Table: React.FC<TableProps> = ({ forms }) => {
+  console.log(forms);
+  return (
+    <>
+      <TableLabels>
+        <Name>
+          <p>NOME</p>
+        </Name>
+        <CreatedAt>
+          <p>CRIADO EM</p>
+        </CreatedAt>
+        <Answers>
+          <p>RESPOSTAS</p>
+        </Answers>
+        <Status>
+          <p>STATUS</p>
+        </Status>
+        <Actions>
+          <p>AÇÕES</p>
+        </Actions>
+      </TableLabels>
+      {forms.map((form) => (
+        <div key={form._id}>
+          <TableRow>
+            <Name>
+              <p>{form.config.name}</p>
+            </Name>
+            <CreatedAt>
+              <p>{form.createdAt}</p>
+            </CreatedAt>
+            <Answers>
+              <p>{form.numResponses} envios</p>
+            </Answers>
+            <Status>
+              <ColorStatus isActive={true} />
+              <p>Ativa</p>
+            </Status>
+            <Actions>
+              <a href="/">
+                <img src={edit} alt="Editar" />
+                <EditLabel>Editar</EditLabel>
+              </a>
+              <div />
+              <a href="/">
+                <img src={trash} alt="Deletar" />
+                <DeleteLabel>Deletar</DeleteLabel>
+              </a>
+            </Actions>
+          </TableRow>
+          <Separator />
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Table;
--- src/services/apoloClient.ts
+++ src/services/apoloClient.ts
@@ -1,15 +1,28 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { setContext } from 'apollo-link-context';
 
-const STAGING_API = 'https://dig-api-staging.herokuapp.com/';
-const PRODUCTION_API = 'https://dig-api-staging.herokuapp.com/';
+const STAGING_URL = 'https://dig-api-staging.herokuapp.com/graphql';
+const PRODUCTION_URL = 'https://dig-api-staging.herokuapp.com/graphql';
 
-export const GRAPHQL_URL =
-  process.env.NODE_ENV === 'production' && process.env.STAGING_APP !== 'true'
-    ? PRODUCTION_API
-    : STAGING_API;
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: process.env.NODE_ENV ? STAGING_URL : PRODUCTION_URL,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  // const token = await getToken();
+  return {
+    headers: {
+      ...headers,
+      // authorization: token ? `Bearer ${token}` : '',
+      authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWNiZDNhZGY3MGE3YTA0Y2FjMzI5YTIiLCJpYXQiOjE1OTA1MjUwMjMsImV4cCI6MTU5MTEyOTgyMywiaXNzIjoiY29udGF0b0Bnb251dmVtLmNvbSJ9.87P0Qeif0F_COluQQ749qdpHtgnmKzAnJ_bHJ5NjS74`,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: GRAPHQL_URL,
+  cache,
+  link: authLink.concat(link),
 });
 
 export default client;
