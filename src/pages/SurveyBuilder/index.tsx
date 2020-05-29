a0e6496e21a34cc3349a47124c115097aac0701d
ppalbuquerque <albpedro897@hotmail.com>
2020-05-29 16:10:18 -0300

    Implemented initial idea for survey builder header

--- src/pages/SurveyBuilder/index.tsx
+++ src/pages/SurveyBuilder/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
-import { Container, Col01, SurveyBuilderHeader } from './styles';
+import { Container, NavLink, SurveyBuilderHeader } from './styles';
 
 import SolidButton from '../../components/Common/SolidButton';
 import LeftPanel from './LeftPanel';
-
-// Col01 component is just for test, erase it.
 
 const SurveyBuilder: React.FC = () => (
   <Container>
     <SurveyBuilderHeader>
       <h5>Pesquisa Eleitoral de Lagoa Alegre</h5>
       <nav>
-        <a href="/">Editar</a>
-        <a href="/">Compartilhar</a>
-        <a href="/">Resultados</a>
+        <NavLink href="/">Editar</NavLink>
+        <NavLink href="/">Compartilhar</NavLink>
+        <NavLink href="/">Resultados</NavLink>
       </nav>
       <SolidButton text="Publicar" />
     </SurveyBuilderHeader>
--- src/pages/SurveyBuilder/styles.ts
+++ src/pages/SurveyBuilder/styles.ts
@@ -1,12 +1,49 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import Colors from '../../utils/colors';
+
+interface NavLinkProps {
+  isActive?: boolean;
+}
 
 export const Container = styled.div``;
 
 export const SurveyBuilderHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
   h5 {
     font-size: 1.3rem;
     font-weight: 700;
+    flex: 1;
   }
+
+  nav {
+    flex: 2;
+  }
+`;
+
+export const NavLink = styled.a<NavLinkProps>`
+  text-decoration: none;
+  color: ${Colors.black};
+
+  transition: color 0.3s;
+
+  & + a {
+    margin-left: 1.5rem;
+  }
+
+  &:hover {
+    color: ${Colors.primary};
+    font-weight: 500;
+  }
+
+  ${(props) => props.isActive
+    && css`
+      font-weight: 500;
+      color: ${Colors.primary};
+    `}
 `;
 
 // Erase this line
