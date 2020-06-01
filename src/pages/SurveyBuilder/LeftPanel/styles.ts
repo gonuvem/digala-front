b3720335f31a943714c6ca4ad8a527075fd34ddf
ppalbuquerque <albpedro897@hotmail.com>
2020-06-01 12:32:47 -0300

    Align questionsContainer in left anchor in left panel

--- src/pages/SurveyBuilder/LeftPanel/styles.ts
+++ src/pages/SurveyBuilder/LeftPanel/styles.ts
@@ -82,7 +82,7 @@ export const PanelTabLink = styled.span<PanelTabLinkProps>`
 export const QuestionsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(112px, 1fr));
-  justify-items: center;
+  justify-items: flex-start;
   grid-gap: 0.5rem;
 
   margin-top: 1rem;
