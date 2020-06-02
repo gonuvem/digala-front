03589c2830dbab395874e414bac5ccc3d53c34f9
ppalbuquerque <albpedro897@hotmail.com>
2020-06-02 11:37:39 -0300

    Added conditional rendering on header component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import SignIn from '../pages/SignIn';
 import MyResearches from '../pages/MyResearches';
 import SurveyBuilder from '../pages/SurveyBuilder';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={MyResearches} />
+    <Route path="/" exact component={SignIn} />
+    <Route path="/my_researches" component={MyResearches} />
     <Route path="/new_survey" component={SurveyBuilder} />
   </Switch>
 );
