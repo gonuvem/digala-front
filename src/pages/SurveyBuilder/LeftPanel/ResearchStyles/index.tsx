f4d1decc37744deea1e02f31fd92c80067991460
Francisco Mendes <fmendes_cn@hotmail.com>
2020-06-02 17:13:59 -0300

    Render styles option

--- src/pages/SurveyBuilder/LeftPanel/ResearchStyles/index.tsx
+++ src/pages/SurveyBuilder/LeftPanel/ResearchStyles/index.tsx
@@ -1,5 +1,68 @@
 import React from 'react';
 
-const ResearchStyles: React.FC = () => <h1>Estilos</h1>;
+import { Container, CardColor, DashedContainer, Section } from './styles';
+
+import uploadIcon from '../../../../assets/uploud_icon.png';
+
+const colors = [
+  { name: 'Amarelo', value: '#FFA825' },
+  { name: 'Vermelho', value: '#EB1D63' },
+  { name: 'Bege', value: '#FFCD80' },
+  { name: 'Verde', value: '#0F5555' },
+  { name: 'Verde Claro', value: '#ADD681' },
+  { name: 'Azul', value: '#3FA4F6' },
+];
+
+const ResearchStyles: React.FC = () => {
+  return (
+    <Container>
+      <Section>
+        <p>Fundo da pesquisa</p>
+        <div>
+          {colors.map((color) => (
+            <a>
+              <CardColor color={color.value} />
+            </a>
+          ))}
+        </div>
+      </Section>
+      <Section>
+        <p>Logo</p>
+        <DashedContainer>
+          <img src={uploadIcon} alt="Upload Logo" />
+          <p>Coloque sua marca aqui</p>
+        </DashedContainer>
+      </Section>
+      <Section>
+        <p>Texto no header</p>
+        <input />
+      </Section>
+      <Section>
+        <p>Logo no header</p>
+        <input />
+      </Section>
+      <Section>
+        <p>Fundo no header</p>
+        <div>
+          {colors.map((color) => (
+            <CardColor color={color.value} />
+          ))}
+        </div>
+      </Section>
+      <Section>
+        <p>Texto no rodapé</p>
+        <input />
+      </Section>
+      <Section>
+        <p>Fundo da pesquisa</p>
+        <div>
+          {colors.map((color) => (
+            <CardColor color={color.value} />
+          ))}
+        </div>
+      </Section>
+    </Container>
+  );
+};
 
 export default ResearchStyles;
--- src/pages/SurveyBuilder/LeftPanel/ResearchStyles/styles.ts
+++ src/pages/SurveyBuilder/LeftPanel/ResearchStyles/styles.ts
@@ -1,1 +1,58 @@
 import styled from 'styled-components';
+
+import Colors from '../../../../utils/colors';
+
+interface CardColorProps {
+  color: string;
+}
+
+export const Container = styled.div`
+  div {
+    /* margin-bottom: 1.5rem; */
+    p {
+      font-weight: 500;
+      margin-bottom: 0.8rem;
+    }
+
+    div {
+      display: flex;
+      flex-wrap: wrap;
+    }
+
+    input {
+      width: 100%;
+      border-radius: 4px;
+      border: solid 1px ${Colors.black};
+      padding: 0.8rem 0 0.8rem 0;
+    }
+  }
+`;
+
+export const CardColor = styled.div<CardColorProps>`
+  height: 3.25rem;
+  width: 3.25rem;
+  border-radius: 4px;
+  margin: 0 0.5rem 0.5rem 0;
+  background: ${(props) => props.color};
+`;
+
+export const DashedContainer = styled.div`
+  /* height: 2rem; */
+  display: flex;
+  padding: 1.5rem;
+  flex-direction: column;
+  place-content: center;
+  border: 1px dashed ${Colors.black};
+  border-radius: 6px;
+
+  img {
+    height: 2rem;
+    width: 2.8125rem;
+    align-self: center;
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const Section = styled.div`
+  margin-bottom: 1.2rem;
+`;
