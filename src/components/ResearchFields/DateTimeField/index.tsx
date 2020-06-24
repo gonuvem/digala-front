39c4e42a2a971c9075341798864f72d80e3f99b2
ppalbuquerque <albpedro897@hotmail.com>
2020-06-24 17:40:11 -0300

    Added function to generate next hours to feed timeSelector

--- src/components/ResearchFields/DateTimeField/index.tsx
+++ src/components/ResearchFields/DateTimeField/index.tsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import { startOfHour, addHours, getHours } from 'date-fns';
 import InputMask from 'react-input-mask';
+import { useTransition, animated } from 'react-spring';
 
 import { Container, InputContainer } from './styles';
 
 const DateTimeField: React.FC = () => {
   const [showTimeSelector, setShowTimeSelect] = useState(false);
+
+  const transitions = useTransition(showTimeSelector, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  const hours: Date[] = useMemo(() => {
+    const now = new Date();
+    const nowStartHour = startOfHour(now);
+
+    const generatedHours = [nowStartHour];
+
+    for (let i = 0; i <= 6; i += 1) {
+      const nextHour = addHours(nowStartHour, i + 1);
+      generatedHours.push(nextHour);
+    }
+
+    return generatedHours;
+  }, []);
 
   return (
     <Container>
@@ -19,12 +41,22 @@ const DateTimeField: React.FC = () => {
         <InputMask mask="99/99/9999" type="text" placeholder="DD/MM/YYYY" />
         <span>:</span>
         <InputContainer>
-          <InputMask mask="99:99:99" type="text" placeholder="Hh:Mm:Ss" />
-          <div>
-            <button type="button">06:00</button>
-            <button type="button">06:00</button>
-            <button type="button">06:00</button>
-          </div>
+          <InputMask
+            onClick={() => setShowTimeSelect((state) => !state)}
+            mask="99:99:99"
+            type="text"
+            placeholder="Hh:Mm:Ss"
+          />
+          {transitions.map(
+            ({ item, key, props }) =>
+              item && (
+                <animated.div key={key} style={props}>
+                  {hours.map((hour) => (
+                    <button type="button">{`${getHours(hour)}:00`}</button>
+                  ))}
+                </animated.div>
+              ),
+          )}
         </InputContainer>
       </div>
     </Container>
