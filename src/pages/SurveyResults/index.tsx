f6ef97a386ec82b1487cffb5b1e3a3cf6aa49bc2
Pedro Henrique <albpedro458@gmail.com>
2020-09-03 12:08:39 -0300

    Added Survey Results page

---
+++ src/pages/SurveyResults/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '../../layout';
+import PageHeader from '../../components/Common/Header';
+
+const SurveyResults: React.FC = () => {
+  return (
+    <>
+      <PageHeader />
+      <Layout />
+    </>
+  );
+};
+
+export default SurveyResults;
