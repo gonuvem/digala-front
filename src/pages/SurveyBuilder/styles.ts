b3fd74d3685f63ebaaf5b9ae388c307368ca2118
ppalbuquerque <albpedro897@hotmail.com>
2020-09-29 17:50:55 -0300

    Fixed alignment on Survey Builder panels

--- src/pages/SurveyBuilder/styles.ts
+++ src/pages/SurveyBuilder/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div``;
 
 export const Panels = styled.div`
   display: flex;
-  align-items: baseline;
+  align-items: flex-start;
 
   & + div {
     margin-left: 2.5rem;
